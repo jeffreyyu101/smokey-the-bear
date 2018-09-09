2ad60f8cff7e39ab133ebc4ffe6f976b538303a9
Jeffrey <jeffreyyu101@gmail.com>
2018-09-09 11:30:20 +0000

    py

--- fb.py
+++ fb.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-print 'Content-type: text/html\n'
-print ''
-import cgi,cgitb, os
-cgitb.enable()
+print "Content-Type: text/html\n\n"
+import cgi, cgitb, os
+
 from fbchat import Client
 from fbchat.models import *
 client = Client('juulythebear@gmail.com','JerryisA1')
-user = client.searchForUsers("Jerry Ye")[0]
+user = client.searchForUsers('Jerry Ye')[0]
 
-message_id = client.send(Message(text="Hi! I'm Juuly The Bear. I am messaging you to tell you that you are on notice for Juuling too much. Using our advanced machine learning software, we have deduced this. Please take notice. Juuling is extremely bad for your health. We would like you to give it up."), thread_id=user.uid, thread_type=ThreadType.USER)
+message_id = client.send(Message(text='I AM JUULY THE BEAR!Im going to take yo\
+ ur JUULES!'), thread_id=user.uid, thread_type=ThreadType.USER)
