fe4359c42aa34a81bed64b84a96bf34dd5783c31
Jeffrey <jeffreyyu101@gmail.com>
2018-09-09 05:12:31 +0000

    fixed

--- .~c9_invoke_I0PIFT.py
+++ .~c9_invoke_I0PIFT.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
-print 'Content-type: text/html\n'
-print ''
-import cgi,cgitb, os
+import cgi,cgitb
 cgitb.enable()
 from fbchat import Client
 from fbchat.models import *
-client = Client('juulythebear@gmail.com','JerryisA1')
-user = client.searchForUsers("Jerry Ye")[0]
+message_id = client.send(Message(text="Hi! I'm Juuly The Bear. I am messaging you to tell "), thread_id=user.uid, thread_type=ThreadType.USER)
+user = client.searchForUsers(ID)[0]
 
 message_id = client.send(Message(text="Hi! I'm Juuly The Bear. I am messaging you to tell you that you are on notice for Juuling too much. Using our advanced machine learning software, we have deduced this. Please take notice. Juuling is extremely bad for your health. We would like you to give it up."), thread_id=user.uid, thread_type=ThreadType.USER)
